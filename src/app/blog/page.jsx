fb9dd8cee851c795a026c161b2f9915c28b07617
Milo Godoy <milogodoy@hotmail.com>
2024-04-17 18:04:15 -0400

    Updated to no-store catching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -6,9 +6,9 @@ async function getData() {
   const domain = getDomain()
   const endpoint = `${domain}/api/posts`
 
-  const res = await fetch(endpoint, {next: {revalidate: 10}})
+  // const res = await fetch(endpoint, {next: {revalidate: 10}})
 
-  // const res = await fetch(endpoint, { cache: 'no-store' })
+  const res = await fetch(endpoint, { cache: 'no-store' })
 
   if(!res.ok) {
     throw new Error("Failed to fetch data")
